8b61e4005e3c936df142b914b74e1efdf299334b
rmfarrell <rmfarrell@gmail.com>
2018-01-18 11:14:38 -0500

    add placeholder variables to repo

---
+++ infrastructure/variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+  default="a_project"
+}
+
+variable "function_name" {
+  default="hello"
+}
+
+variable "account_id" {
+  default="0000000000"
+}
+
+variable "aws_region" {
+  default="TF_VAR_aws_region"
+}
+
+variable "aws_region" {
+  default="TF_VAR_aws_region"
+}
+
+variable "apex_function_proxy" {
+  default="arn:aws:lambda:us-east-1:0000000000:function:a_project_hello"
+}
